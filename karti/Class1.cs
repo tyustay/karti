846217d39d6447aa886fbd2a6052ccc441e2b11e
tyustay _ <rinjifukuda1999@gmail.com>
2022-02-14 14:14:10 +0300

    Добавьте файлы проекта.

---
+++ karti/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LinqFaroShuffle;
+
+namespace karti
+
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> LogQuery<T>
+        (this IEnumerable<T> sequence, string tag)
+        {
+            // File.AppendText creates a new file if the file doesn't exist.
+            using (var writer = File.AppendText("debug.log"))
+            {
+                writer.WriteLine($"Executing Query {tag}");
+            }
+            return sequence;
+        }
+    }
+}
---
+++ karti/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LinqFaroShuffle;
+using karti;
+
+namespace LinqFaroShuffle
+
+{
+    public static class Extensions
+
+    {
+
+        public static IEnumerable<T> InterleaveSequenceWith<T>
+
+        (this IEnumerable<T> first, IEnumerable<T> second)
+
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+
+        public static bool SequenceEquals<T>
+    (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+
+}
+
+namespace dev_rus
+{
+    class Program
+    {
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+        // Program.cs
+
+        public static void Main(string[] args)
+        {
+            var startingDeck = (from s in Suits().LogQuery("Suit Generation")
+                                from r in Ranks().LogQuery("Value Generation")
+                                select new { Suit = s, Rank = r })
+                                .LogQuery("Starting Deck")
+                                .ToArray();
+
+            foreach (var c in startingDeck)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine();
+
+            var times = 0;
+            var shuffle = startingDeck;
+
+            do
+            {
+                /*
+                shuffle = shuffle.Take(26)
+                    .LogQuery("Top Half")
+                    .InterleaveSequenceWith(shuffle.Skip(26).LogQuery("Bottom Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
+                */
+
+                shuffle = shuffle.Skip(26)
+                    .LogQuery("Bottom Half")
+                    .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
+
+                foreach (var c in shuffle)
+                {
+                    Console.WriteLine(c);
+                }
+                times++;
+                Console.WriteLine(times);
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+            Console.ReadKey();
+        }
+        
+    } 
+} 
